e62dcb2b9421a8d88532a8e8cdf457239bbcc4e7
yaozhang0104 <zhangdazzle@gmail.com>
2016-11-15 20:21:32 -0800

    Minor UI

--- ParkingFinderFront/src/components/Settings.js
+++ ParkingFinderFront/src/components/Settings.js
@@ -2,7 +2,38 @@ import React, { PropTypes } from 'react'
 import { StyleSheet, Text, TouchableOpacity, Modal, View } from 'react-native';
 
 const styles = StyleSheet.create({
-
+    mainViewContainer: {
+      flex: 1, 
+      flexDirection:'column',
+    },
+    headerContainer: {
+      flex: 0.1,
+      backgroundColor: '#F9F9F9',
+      borderBottomWidth: 1,
+      borderBottomColor: '#B2B2BA',
+      alignItems: 'center', 
+      flexDirection: 'row', 
+      justifyContent: 'center',
+    },
+    headerText: {
+      fontFamily: 'Helvetica',
+      fontSize: 18,
+      fontWeight: '200',
+      marginTop: 15
+    },
+    cancel: {
+      position: 'absolute',
+      left: 20,
+      top: 28,
+    },
+    cancelText: {
+      fontFamily: 'Helvetica',
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    infoContainer: {
+      flex: 0.9,
+    }
 });
 
 const Settings = (props) => {
@@ -12,15 +43,19 @@ const Settings = (props) => {
     <Modal
       animationType='slide'
       visible={visible}>
-        <View style={[styles.container]}>
-          <View style={[styles.innerContainer]}>
-            <Text>Settings</Text>
+        <View style={styles.mainViewContainer}>
+          <View style={styles.headerContainer}>
             <TouchableOpacity
+              style={styles.cancel}
               onPress={requestClose}>
-            <Text>X</Text>
+              <Text style={styles.cancelText}>X</Text>
             </TouchableOpacity>
+            <Text style={styles.headerText}>SETTINGS</Text>
           </View>
-        </View>
+          <View style={styles.infoContainer}>
+
+          </View>
+      </View>
     </Modal>
   )
 }
